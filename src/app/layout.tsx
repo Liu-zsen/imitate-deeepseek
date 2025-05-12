e6ccdd5d2aa625751d71fb24a2ebf5d1b7efab9b
Liu-zsen <773853230@qq.com>
2025-05-12 16:43:10 +0800

    ai聊天 first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import {
+  ClerkProvider,
+} from '@clerk/nextjs'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,12 +26,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
+      
     <html lang="en">
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div className="w-1/5 h-screen bg-gray-50"></div>
         {children}
       </body>
     </html>
+
+    </ClerkProvider>
   );
 }
